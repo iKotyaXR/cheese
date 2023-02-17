ccb76c2ecea287c49a45d4ae1690c2b868e124a9
iKotyaXR <iKotyaXR@yandex.ru>
2023-02-18 00:36:32 +0300

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import '../scss/style.scss'
+import Swiper, { Navigation, Pagination } from 'swiper'
+Swiper.use([Navigation, Pagination])
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  centeredSlides: false,
+  centeredSlidesBounds: true,
+  watchOverflow: true,
+  direction: 'horizontal',
+  slidesPerView: 2.4,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+})
