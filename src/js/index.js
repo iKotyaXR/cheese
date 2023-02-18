7962da95730ae83158fbd5b2412ed5c582a2999c
iKotyaXR <iKotyaXR@yandex.ru>
2023-02-18 17:50:39 +0300

    end fuooter

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,11 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 2.4,
   spaceBetween: 20,
   lazy: true,
+  observer: true,
+  observeParents: true,
+  keyboard: {
+    enabled: true
+  },
   breakpoints: {
     500: {
       slidesPerView: 3
